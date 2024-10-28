8d9d5c74874b18f664f6c9ddaab1faafdcb50f40
EmilTsaturyan <emiltsaturyan60@gmail.com>
2024-10-28 17:32:11 +0400

    FIX BUG

--- rpc/__init__.py
+++ rpc/__init__.py
@@ -1,7 +1,7 @@
 from solana.rpc.async_api import AsyncClient
 
 
-RPC = 'https://solana-api.instantnodes.io/token-DBZogRbqut06bL19vTd2LLkSkREYOSk6'
-# RPC = 'https://api.devnet.solana.com'
+# RPC = 'https://solana-api.instantnodes.io/token-DBZogRbqut06bL19vTd2LLkSkREYOSk6'
+RPC = 'https://api.devnet.solana.com'
 client = AsyncClient(RPC)
 metis_client = AsyncClient('https://crimson-proud-putty.solana-mainnet.quiknode.pro/5c1e774b29d63fc71ca7f010f5547353134b3bdc')
