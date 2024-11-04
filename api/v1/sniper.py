e4a5d409a68e1b07ca0e1ba5d2bb315d6a52f5db
Vardan <vardanter@yandex.ru>
2024-11-04 14:15:42 +0400

    Add orders endpoint

--- api/v1/sniper.py
+++ api/v1/sniper.py
@@ -8,21 +8,36 @@ from bot import bot
 from db import database
 
 
-router = APIRouter(tags=['Sniper'])
+router = APIRouter(tags=["Sniper"])
 
 
-@router.post('/notificate')
-async def notificate_sniper(sniper_notificate: SniperNotificate, session: AsyncSession = Depends(database.get_async_session), validate = Depends(validate.sniper_service_validate)):
+@router.post("/notificate")
+async def notificate_sniper(
+    sniper_notificate: SniperNotificate,
+    session: AsyncSession = Depends(database.get_async_session),
+    validate=Depends(validate.sniper_service_validate),
+):
     order = await sniper.get_order_by_id(sniper_notificate.order_id, session)
 
     confirmed = sniper_notificate.confirmed
     if confirmed:
-        confirmation_text = 'Sniper went successfully'
+        confirmation_text = "Sniper went successfully"
     else:
-        confirmation_text = 'Sniper failed'
+        confirmation_text = "Sniper failed"
 
     await bot.send_message(
         chat_id=sniper_notificate.user_id,
-        text=f'{confirmation_text}\nYou bought {sniper_notificate.token_amount} for {order.sol_amount}'
+        text=f"{confirmation_text}\nYou bought {sniper_notificate.token_amount} for {order.sol_amount}",
     )
     await session.delete(order)
+
+
+@router.get("/orders/{token_address}")
+async def active_orders(
+    token_address: str,
+    session: AsyncSession = Depends(database.get_async_session),
+    validate=Depends(validate.sniper_service_validate),
+):
+    orders = await sniper.get_orders_by_token(token_address, session)
+
+    return orders
--- utils/sniper.py
+++ utils/sniper.py
@@ -4,17 +4,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from core import settings
-from models import Order
+from models import Order, SolanaWallet
 
 
-
-async def create_order(
-        session: AsyncSession,
-        **fields
-    ):
-    order = Order(
-        **fields
-    )
+async def create_order(session: AsyncSession, **fields):
+    order = Order(**fields)
     session.add(order)
     await session.commit()
     return order
@@ -32,52 +26,88 @@ async def get_order_by_id(order_id: int, session: AsyncSession):
     return order
 
 
+async def get_orders_by_token(token_address: str, session: AsyncSession):
+    stmt = (
+        select(
+            Order.sol_amount,
+            Order.slippage,
+            Order.mev_protection,
+            Order.id,
+            Order.token_address,
+            Order.gas,
+            SolanaWallet.wallet,
+            SolanaWallet.encrypted_private_key,
+        )
+        .join(SolanaWallet, SolanaWallet.id == Order.wallet)
+        .filter(Order.token_address == token_address)
+    )
+    result = await session.execute(stmt)
+    orders = result.fetchall()
+
+    return [
+        {
+            "order_id": order.id,
+            "token_address": order.token_address,
+            "sol_amount": order.sol_amount,
+            "slippage": order.slippage,
+            "mev_protection": order.mev_protection,
+            "gas": order.gas,
+            "user_id": order.wallet,
+            "private_key": order.encrypted_private_key.decode(),
+        }
+        for order in orders
+    ]
+
+
 async def remove_order(order_id: int, session: AsyncSession):
     order = await session.get(Order, order_id)
     await session.delete(order)
     await session.commit()
 
 
-
-async def create_order_service(user_id, order: Order, private_key: bytes, session: ClientSession):
+async def create_order_service(
+    user_id, order: Order, private_key: bytes, session: ClientSession
+):
     data = {
         "user_id": user_id,
         "order_id": order.id,
         "token_address": order.token_address,
         "slippage": order.slippage,
-        "mev_protection": order.mev_protection, 
+        "mev_protection": order.mev_protection,
         "gas": order.gas,
         "private_key": private_key.decode(),
-        'sol_amount': order.sol_amount
+        "sol_amount": order.sol_amount,
     }
-    headers = {
-        'x-access-token': settings.sniper_access_token
-    }
-    async with session.post('http://sniper:9000/snipe', json=data, headers=headers) as response:
+    headers = {"x-access-token": settings.sniper_access_token}
+    async with session.post(
+        "http://sniper:9000/snipe", json=data, headers=headers
+    ) as response:
         response = await response.json()
 
 
-async def update_order_service(user_id, order: Order, private_key: bytes, session: ClientSession):
+async def update_order_service(
+    user_id, order: Order, private_key: bytes, session: ClientSession
+):
     data = {
         "user_id": user_id,
         "order_id": order.id,
         "token_address": order.token_address,
         "slippage": order.slippage,
-        "mev_protection": order.mev_protection, 
+        "mev_protection": order.mev_protection,
         "gas": order.gas,
         "private_key": private_key.decode(),
-        'sol_amount': order.sol_amount
+        "sol_amount": order.sol_amount,
     }
-    headers = {
-        'x-access-token': settings.sniper_access_token
-    }
-    async with session.patch(f'http://sniper:9000/snipe/{order.id}', json=data, headers=headers) as response:
+    headers = {"x-access-token": settings.sniper_access_token}
+    async with session.patch(
+        f"http://sniper:9000/snipe/{order.id}", json=data, headers=headers
+    ) as response:
         response = await response.json()
 
 
 async def remove_order_service(order_id: int, session: ClientSession):
-    headers = {
-        'x-access-token': settings.sniper_access_token
-    }
-    async with session.delete(f'http://sniper:9000/snipe/{order_id}', headers=headers) as response:
+    headers = {"x-access-token": settings.sniper_access_token}
+    async with session.delete(
+        f"http://sniper:9000/snipe/{order_id}", headers=headers
+    ) as response:
         response = await response.json()
